57f8a4c69b2c4f96a294ee34ec4f44eda58ef45c
Suman Saha <sumanrider7@gmail.com>
2025-01-23 16:21:38 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+
+        <div>
+          <h1 className='text-4xl font-semibold text-blue-900 pb-2'>NextCura</h1>
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur quia quas pariatur doloribus magnam tempora a dolorem sint, ducimus asperiores illum recusandae repellendus possimus quasi atque fugiat.</p>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2 text-gray-600 cursor-pointer'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Contact us</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-1 text-gray-600'>
+            <li>+1-212-456-7890</li>
+            <li>sumansaha1108@gmail.com</li>
+          </ul>
+        </div>
+      </div >
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2024@ NextCura - All Rights Reserved.</p>
+      </div>
+    </div >
+  )
+};
+
+export default Footer;
